4a607e608ea593da9f09694e3252a328f80d745b
Glen <glen@mailoo.org>
2015-11-05 00:21:24 +0100

    javascript presentation of extraction

--- server.py
+++ server.py
@@ -13,10 +13,40 @@ app_folder = os.path.split(os.path.abspath(__file__))[0]
 
 @app.route('/_target')
 def target():
-    """return info from url"""
+    """return info from url AJAX way"""
     target_url = request.args.get('target_url', 'None', type=str)
     info = extract_profile(target_url, providers)
-    return jsonify(target_url=target_url, info=str(info))
+    if info != None:
+        print("YOOOOOOO")
+        # is this profile matching an item in db?
+        if False:
+            return jsonify(profile_found=True,
+                target_url=target_url,
+                name=info['name'],
+                photo_url=info['photo_url'],
+                domain=info['domain'],
+                contact_found=True,
+                contact_info='0102032156')
+        else:
+            return jsonify(profile_found=True,
+                target_url=target_url,
+                name=info['name'],
+                photo_url=info['photo_url'],
+                domain=info['domain'],
+                contact_found=False)
+    else:
+        return jsonify(profile_found=False,
+            target_url=target_url)
+
+@app.route('/target')
+def result():
+    """return info from url"""
+    target_url = request.args.get('url', 'None', type=str)
+    info = extract_profile(target_url, providers)
+    return render_template('result.html',
+        target_url=target_url,
+        info=str(info))
+
 
 @app.route('/')
 def main_page():
--- static/js/general.js
+++ static/js/general.js
@@ -1,6 +1,41 @@
-function toggle(source, name) {
-  checkboxes = document.getElementsByClassName(name);
-  for(var i=0, n=checkboxes.length;i<n;i++) {
-    checkboxes[i].checked = source.checked;
-  }
-}
+function show_result(data) {
+    //react to server data
+    //hide spinner
+    $('#spinner').fadeOut("fast");
+    if (data.profile_found) {
+        //fill user id panel
+        $('#tname').text(data.name);
+        $('#tdomain').text(data.domain);
+        $('#tphoto').attr('src', data.photo_url);
+        
+        //is the profile associated with a contact?
+        if (data.contact_found) {
+            $('#tcontact').text(data.contact_info)
+        } else {
+            $('#add_contact').fadeIn("slow");
+        }
+        //hide error
+        $('#bad_url').fadeOut("fast");
+        //show user id panel
+        $('#information').fadeIn("slow");
+    }
+    else {
+        //hide panel
+        $('#information').fadeOut("fast");
+        //show error
+        console.info(data);
+        $('#turl').text(data.target_url);
+        $('#bad_url').fadeIn("slow");
+    }
+};
+
+ function submit_form(e) {
+    //show spinner
+    $('#spinner').fadeIn("fast");
+    //send form as json
+    $.getJSON($SCRIPT_ROOT + '/_target', {
+    target_url: $('input[name="target_url"]').val(),
+    } , show_result);
+    return false;
+};
+
